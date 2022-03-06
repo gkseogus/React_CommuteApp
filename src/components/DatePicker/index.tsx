f284472ec86041ee67a7f889e60c8bf2e9a7634b
gkseogus <fbznffldj998@naver.com>
2022-03-06 14:48:10 +0900

    220306_DatePicker

--- src/components/DatePicker/index.tsx
+++ src/components/DatePicker/index.tsx
@@ -1,48 +1,15 @@
-import React, { useEffect, useState } from "react";
-import DatePicker from "react-datepicker";
-import styled from "styled-components";
-import "react-datepicker/dist/react-datepicker.css";
+import React from 'react';
+import 'antd/dist/antd.css';
+import { DatePicker, Space } from 'antd';
 
-const SectionTitle = styled.div`
-  height: 100%;
-  width: 100%; 
-  padding: 20px;
-  background: red;
-`;
+const { RangePicker } = DatePicker;
 
+const HomeDatePicker = (_props: any) => {
+  return(
+  <Space direction="vertical" size={12}>
+    <RangePicker />
+  </Space>
+  )
+}
 
-type AllProps = any;
-const DatePickerComponent: React.FC<AllProps> = () => {
-  const [startDate, setStartDate] = useState(new Date());
-  const [endDate, setEndDate] = useState(new Date());
-  const [startDat1e, setStartDate1] = useState(new Date());
-
-  return (
-      <SectionTitle>
-        <DatePicker 
-          selected={startDate} 
-          onChange={(date:Date) => setStartDate(date)} 
-          selectsStart
-          startDate={startDate}
-          endDate={endDate}
-        />
-        <DatePicker 
-          selected={endDate} 
-          onChange={(date:Date) => setEndDate(date)} 
-          selectsEnd
-          startDate={startDate}
-          endDate={endDate}
-          minDate={startDate}
-        />
-        <DatePicker
-          selected={startDat1e}
-          onChange={(date:Date) => setStartDate1(date)}
-          isClearable
-          placeholderText="I have been cleared!"
-        />
-      </SectionTitle>
-  );
-};
-
-
-export default DatePickerComponent ;
+export default HomeDatePicker
