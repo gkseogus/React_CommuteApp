103641bcb11294407563da62463f3b122cfbfdc4
gkseogus <fbznffldj998@naver.com>
2022-03-10 15:19:53 +0900

    220310_routes

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,21 +1,14 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
-// import HomePage from './components/HomePage';
-import HomeDatePicker from './components/DatePicker';
-import UserSearch from './components/UserSearch';
+import HomePage from './components/HomePage';
 
 const Routes: React.SFC = () => (
   <div>
     {/* 첫 번째 route */}
-    <Route
-      exact
-      path="/"
-      render={() => (
-        <div>
-        <HomeDatePicker/>
-        <UserSearch/>
-        </div>
-      )}
+    <Route exact 
+      path="/" 
+      render={() => 
+        <HomePage />} 
     />
   </div>
 );
