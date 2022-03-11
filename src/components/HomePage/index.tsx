851e29414f7aeb9140d8ec57e7dd2ad9a1b0e5d1
gkseogus <fbznffldj998@naver.com>
2022-03-11 17:08:16 +0900

    220311_모듈삭제

--- src/components/HomePage/index.tsx
+++ src/components/HomePage/index.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import 'antd/dist/antd.css';
 import { Table } from 'antd';
 import { useSelector } from 'react-redux';
