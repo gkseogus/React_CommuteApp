f994e5ec1a598cdcd0a59d56d70956f23d891dc9
gkseogus <fbznffldj998@naver.com>
2022-03-14 16:40:22 +0900

    220314_api변경

--- src/components/GoogleLogin/index.tsx
+++ src/components/GoogleLogin/index.tsx
@@ -122,7 +122,7 @@ export const AuthController = (_props: any) => {
     setLoginUserName(res.Ju.sf)
     try {
       const res2 = await Promise.allSettled([fetch(
-          'https://api.apispreadsheets.com/data/YN1QAPcdoAu294nX/'
+          'https://api.apispreadsheets.com/data/6N3SdZx9voMzw0Bc/'
           ,{
               method: 'POST',
               headers: {
