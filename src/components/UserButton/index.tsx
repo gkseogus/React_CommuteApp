ad537362867a56af46aee4bc4b81c51192d46c13
gkseogus <fbznffldj998@naver.com>
2022-03-14 10:11:31 +0900

    220314_SyntaxError 해결

--- src/components/UserButton/index.tsx
+++ src/components/UserButton/index.tsx
@@ -28,34 +28,10 @@ const UserButton = (_props: any) => {
 
     const btnDisable = async () => {
         setDisable(true)
+
         // 데이터 삽입을 위한 format 
         const newDate = moment(new Date()).format('YYYY DD월 MM일,HH:mm:ss');  
         console.log('출근시간',newDate);
-        try {
-            const res = await fetch(
-                'https://api.apispreadsheets.com/data/wpeGmpNSay3cBnH5/'
-                ,{
-                    method: 'POST',
-                    headers: {
-                        'Content-Type': 'application/json'
-                    },
-                    body: JSON.stringify({"data":
-                {"checkIn": checkInState.checkIn}
-                })
-                }
-            );
-            await res.json();
-            setCheckInState({...checkInState, checkIn:newDate})
-        } catch(err){
-            console.log('error:', err);
-        }
-    }
-
-    const reverseDisable = async () => {
-        setDisable(false)
-        const newDate = moment(new Date()).format('YYYY DD월 MM일,HH:mm:ss');
-        console.log('퇴근시간',newDate);
-        // console.log('test', moment.duration(checkInState.diff(checkInState)).asHours())
         try {
             const res = await fetch(
                 'https://api.apispreadsheets.com/data/1Hu1GF1mNrXIVgOt/'
@@ -65,11 +41,43 @@ const UserButton = (_props: any) => {
                         'Content-Type': 'application/json'
                     },
                     body: JSON.stringify({"data":
-                {"checkOut": checkOutState.checkOut}
+                {"checkIn": checkInState.checkIn, "working": "출근"}
                 })
                 }
             );
-            await res.json();
+            console.log(res);
+            setCheckInState({...checkInState, checkIn:newDate})
+        } catch(err){
+            console.log('error:', err);
+        }
+    }
+
+    const reverseDisable = async () => {
+        setDisable(false)
+        
+        // 단순 api에 데이터를 보내주기 위한 변수
+        const newDate = moment(new Date()).format('YYYY DD월 MM일,HH:mm:ss'); 
+        console.log('퇴근시간',newDate);
+
+        // (퇴근시간 - 출근시간) 연산을 위한 변수
+        const wenDate = moment();
+        console.log('wEN',wenDate)
+        console.log('rere',checkInState.checkIn)
+        console.log('test', moment.duration(wenDate.diff(checkInState.checkIn)).asHours())
+        try {
+            const res = await fetch(
+                'https://api.apispreadsheets.com/data/1Hu1GF1mNrXIVgOt/'
+                ,{
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify({"data":
+                {"checkOut": checkOutState.checkOut, "working": "퇴근"}
+                })
+                }
+            );
+            console.log(res);
             setCheckOutState({...checkOutState, checkOut:newDate})
         } catch(err){
             console.log('error:', err);
