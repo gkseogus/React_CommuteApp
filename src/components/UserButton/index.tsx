aa1b2f8df540d05de3c6ef239267d532a4f57fbf
gkseogus <fbznffldj998@naver.com>
2022-03-15 10:03:15 +0900

    220315_상태값, api 삭제 및 코드 업데이트

--- src/components/UserButton/index.tsx
+++ src/components/UserButton/index.tsx
@@ -16,74 +16,40 @@ const Container2 = styled.div`
 `;
 
 const UserButton = (_props: any) => {
-    
+    // 버튼 활성화 상태 값
     const [disable, setDisable] = useState(false);
 
+    // 출근 상태 값 (reverseDisable에 넣어주기 위함)
     const [checkInState, setCheckInState] = useState({
-        checkIn: moment(new Date()).format('YYYY MM월 DD일,HH:mm:ss')
-    });
-    const [checkOutState, setCheckOutState] = useState({
-        checkOut: moment(new Date()).format('YYYY MM월 DD일,HH:mm:ss')
+        checkIn: moment().format('YYYY MM월 DD일,HH:mm:ss')
     });
     
+    // 근무 상태 값
     const [workState, setWorkState] = useState('');
 
     // 단순 연산을 위한 상태값(format x)
     const [workTime, setWorkTime] = useState({});
 
-    const btnDisable = async () => {
+    const btnDisable = () => {
+        const attendanceDate = moment(new Date()).format('YYYY MM월 DD일,HH:mm:ss'); 
+        console.log('출근시간',attendanceDate);
+
         setDisable(true);
         // 출근버튼 클릭 시 workTime에 출근시간 값 저장
         setWorkTime(moment((new Date())));
-
-        // 데이터 삽입을 위한 format 
-        const attendanceDate = moment(new Date()).format('YYYY MM월 DD일,HH:mm:ss'); 
-        console.log('출근시간',attendanceDate);
-        try {
-            const res = await Promise.allSettled([fetch(
-                'https://api.apispreadsheets.com/data/6N3SdZx9voMzw0Bc/'
-                ,{
-                    method: 'POST',
-                    headers: {
-                        'Content-Type': 'application/json'
-                    },
-                    body: JSON.stringify({'data':
-                {'checkIn': checkInState.checkIn, 'working': '출근'}
-                })
-                }
-            )]);
-            console.log(res);
-            setCheckInState({...checkInState, checkIn:attendanceDate});
-        } catch(err){
-            console.log('error:', err);
-        }
+        setCheckInState({...checkInState, checkIn:attendanceDate});
     }
 
     const reverseDisable = async () => {
-        setDisable(false);
-        
         const leaveDate = moment(new Date()).format('YYYY MM월 DD일,HH:mm:ss'); 
         console.log('퇴근시간',leaveDate);
 
-        // 현재시간에서 9시간 뺀 값
-        // const subHours = moment(new Date()).subtract(9, 'h').format('YYYY MM월 DD일,HH:mm:ss');
-        
         // 단순 연산을 위한 변수(format x)
         const leaveDate2 = Number(moment(new Date()));
         // 퇴근시간 - 출근시간 
         const subtract = leaveDate2-Number(workTime);
-
-        // subHours의 시간은 현재시간에서 9시간 뺀 값
-        // 즉 정상 근무를 하였으면 퇴근시간 - 9시간 = 출근시간 
-        // 만약 출근시간이랑 동일하지 않으면 그것은 근무미달
-        // if(subHours !== checkInState.checkIn){
-        //     console.log('근무미달');
-        //     setWorkState('근무미달');
-        // }
-        // else {
-        //     console.log('정상');
-        //     setWorkState('정상');  
-        // }
+        
+        setDisable(false);
 
         // subtract에는 퇴근시간 - 출근시간 의 값이 들어있다.
         // 이 값이 3.24e+7(9시간을 ms로 환산한 값, 32400000)보다 작으면 근무미달
@@ -97,7 +63,7 @@ const UserButton = (_props: any) => {
             setWorkState('정상');
         }
         try {
-            const res = await Promise.allSettled([fetch(
+            const res = await fetch(
                 'https://api.apispreadsheets.com/data/6N3SdZx9voMzw0Bc/'
                 ,{
                     method: 'POST',
@@ -106,13 +72,13 @@ const UserButton = (_props: any) => {
                     },
                     body: JSON.stringify({'data':
                     {
-                        'checkOut': checkOutState.checkOut, 'working': '퇴근', 'workTime': subtract, 'workState': workState
+                        'checkIn': checkInState.checkIn, 'checkOut': leaveDate, 'working': '퇴근', 
+                        'workTime': subtract, 'workState': workState
                     }
                 })
                 }
-            )]);
+            );
             console.log(res);
-            setCheckOutState({...checkOutState, checkOut:leaveDate});
         } catch(err){
             console.log('error:', err);
         }   
