8318d8d9d1d0fd6825bc839b894600ca92ca174b
gkseogus <fbznffldj998@naver.com>
2022-03-16 15:10:13 +0900

    220316_datePicker 업데이트

--- src/components/DatePicker/index.tsx
+++ src/components/DatePicker/index.tsx
@@ -2,14 +2,11 @@ import React from 'react';
 import 'antd/dist/antd.css';
 import { DatePicker, Space } from 'antd';
 
-const { RangePicker } = DatePicker;
-
 const HomeDatePicker = (_props: any) => {
   return (
     <Space key={'DP'} direction='vertical' size={12}>
-    <RangePicker
-      showTime={{ format: 'HH:mm' }}
-      format='YYYY-MM-DD HH:mm'
+    <DatePicker
+      format='YYYY-MM-DD'
       onChange={_props.onChange}
     />
     </Space>
