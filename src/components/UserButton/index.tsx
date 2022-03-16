51f21161b78672e9a46d8a863a21420844f953a1
gkseogus <fbznffldj998@naver.com>
2022-03-16 10:22:26 +0900

    220316_로그인 해야 버튼이 활성화

--- src/components/UserButton/index.tsx
+++ src/components/UserButton/index.tsx
@@ -4,6 +4,7 @@ import styled from 'styled-components';
 import { Button } from 'antd';
 import moment from 'moment';
 import { useState } from 'react';
+import { useEffect } from 'react';
 
 const Container = styled.div`
     position: fixed;
@@ -20,8 +21,9 @@ declare const window: Window & {
 };
 
 const UserButton = (_props: any) => {
-    // 버튼 활성화 상태 값
-    const [disable, setDisable] = useState(false);
+    // 버튼의 disable 활성화 상태 값
+    const [disableBtn, setDisableBtn] = useState(true);
+    const [disableRevers, setDisableRevers] = useState(true);
 
     // 출근 상태 값 (reverseDisable에 넣어주기 위함)
     const [checkInState, setCheckInState] = useState({
@@ -33,23 +35,22 @@ const UserButton = (_props: any) => {
 
     // 근무 상태 값
     const [workState, setWorkState] = useState('근무미달');
-    
 
     const btnDisable =  async () => {
         const attendanceDate = moment().format('YYYY MM월 DD일,HH:mm:ss'); 
         console.log('출근시간',attendanceDate);
-        setDisable(true);
 
+        setDisableBtn(true);
+        setDisableRevers(false);
+        
         // 출근버튼 클릭 시 workTime에 출근시간 값 저장
         setWorkTime(moment((new Date())));
         setCheckInState({...checkInState, checkIn:attendanceDate});
-        // 데이터저장
-        // window.localStorage.setItem('workTime', String(workTime));
-        console.log(window.localStorage)
+
         try {
             // eslint-disable-next-line @typescript-eslint/no-unused-vars
             const res = await fetch(
-                'https://api.apispreadsheets.com/data/TUQgtSPZy4hYo8W1/'
+                'https://api.apispreadsheets.com/data/FjiaXFDCsTRfopfP/'
                 ,{
                     method: 'POST',
                     headers: {
@@ -76,17 +77,19 @@ const UserButton = (_props: any) => {
         const leaveDate = moment(new Date()).format('YYYY MM월 DD일,HH:mm:ss'); 
         console.log('퇴근시간',leaveDate);
 
+        setDisableBtn(true);
+        setDisableRevers(true);
+
         // 단순 연산을 위한 변수(format x)
         const leaveDate2 = Number(moment(new Date()));
+
         // 퇴근시간 - 출근시간 
         const subtractHour = Math.floor(((leaveDate2 - Number(workTime))/1000)/60/60);
         const subtractMinute = Math.floor(((leaveDate2 - Number(workTime))/1000)/60);
         const subtractSecond = Math.floor(((leaveDate2 - Number(workTime))/1000));
-        setDisable(false);
 
         // subtractHour에는 퇴근시간 - 출근시간 의 시 값이 들어있다.
         // 이 값이 3.24e+7(9시간을 ms로 환산한 값, 32400000)보다 작으면 근무미달
-        // 그 외의 나머지 조건은 모두 정상
         if (subtractHour < 3.24e+7) {
             console.log('근무시간:',subtractHour,'시간',subtractMinute,'분',subtractSecond,'초',' 근무미달');
             setWorkState('근무미달');
@@ -102,7 +105,7 @@ const UserButton = (_props: any) => {
         try {
             // eslint-disable-next-line @typescript-eslint/no-unused-vars
             const res = await fetch(
-                'https://api.apispreadsheets.com/data/TUQgtSPZy4hYo8W1/'
+                'https://api.apispreadsheets.com/data/FjiaXFDCsTRfopfP/'
                 ,{
                     method: 'POST',
                     headers: {
@@ -117,23 +120,30 @@ const UserButton = (_props: any) => {
                         'working': '퇴근'
                     },
                     // 쿼리문을 사용해 데이터 업데이트 
-                    "query": `select*from23775wherecheckIn='${checkInState.checkIn}'`
+                    "query": `select*from23805wherecheckIn='${checkInState.checkIn}'`
                 })
                 }
             );
-            // window.location.reload();
+            window.location.reload();
         } catch(err){
             console.log('error:', err);
         }   
     }
 
+    useEffect(() => {
+        if(window.localStorage.user_name !== undefined){
+            setDisableBtn(false);
+            setDisableRevers(true);
+        }
+    },[])
+
     return (
         <div key={'UB'}>
             <Container>
-                <Button type='primary' disabled={disable} onClick={btnDisable}>출근</Button>
+                <Button type='primary' disabled={disableBtn} onClick={btnDisable}>출근</Button>
             </Container>
             <Container2>
-                <Button type='primary' disabled={!disable} onClick={reverseDisable}>퇴근</Button>
+                <Button type='primary' disabled={disableRevers} onClick={reverseDisable}>퇴근</Button>
             </Container2>
         </div>
     );
