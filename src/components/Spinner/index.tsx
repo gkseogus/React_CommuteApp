4254ba9a189fd5d3a6441a588d8027996cd80663
gkseogus <fbznffldj998@naver.com>
2022-03-17 15:41:44 +0900

    220317_spinner

---
+++ src/components/Spinner/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import 'antd/dist/antd.css';
+import { Spin } from 'antd';
+
+const Spinner = (_props: any) => {
+  return (
+    <div className="example">
+      <Spin/>
+    </div>
+  );
+};
+
+export default Spinner;
