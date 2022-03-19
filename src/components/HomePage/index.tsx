bbee9f58ad37050433c5b40e3189295a6943a0c1
gkseogus <fbznffldj998@naver.com>
2022-03-19 23:36:28 +0900

    220319_homePage 컴포넌트 업데이트

--- src/components/HomePage/index.tsx
+++ src/components/HomePage/index.tsx
@@ -1,13 +1,13 @@
-import React, { useState } from 'react';
-import 'antd/dist/antd.css';
 import { Table } from 'antd';
+import 'antd/dist/antd.css';
+import { Moment } from 'moment';
+import React, { useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
 import { ApplicationState } from '../../store';
 import HomeDatePicker from '../DatePicker';
+import { checkGapi, converToState, getSheet } from '../GoogleLogin/load-sheet';
 import UserButton from '../UserButton';
 import UserSearch from '../UserSearch';
-import { Moment } from 'moment';
-import moment from 'moment';
 
 const columns: any = [
   {
@@ -53,16 +53,16 @@ const columns: any = [
       {
         text: 'CM',
         value: 'CM',
-      },      
+      },
       {
         text: '경영지원',
-        value: '경영지원'
-      }
+        value: '경영지원',
+      },
     ],
     // specify the condition of filtering result
     // here is that finding the name started with `value`
     onFilter: (value: any, record: { team: string | any[] }) =>
-      record.team.indexOf(value) === 0
+      record.team.indexOf(value) === 0,
   },
   {
     title: '사용자',
@@ -70,24 +70,24 @@ const columns: any = [
     defaultSortOrder: 'user',
     sorter: (a: { user: string }, b: { user: string }) => {
       // 삼항연산자, a<b이면 1, elseIf(a===b)이면 0, else이면 -1
-      return (a<b)?1 : (a===b)?0:-1;
-    }
+      return a < b ? 1 : a === b ? 0 : -1;
+    },
   },
   {
     title: '체크인',
     dataIndex: 'checkIn',
     defaultSortOrder: 'checkIn',
     sorter: (a: { checkIn: string }, b: { checkIn: string }) => {
-      return (a.checkIn<b.checkIn)?1 : (a.checkIn===b.checkIn)?0:-1;
-    }
+      return a.checkIn < b.checkIn ? 1 : a.checkIn === b.checkIn ? 0 : -1;
+    },
   },
   {
     title: '체크아웃',
     dataIndex: 'checkOut',
     defaultSortOrder: 'checkOut',
     sorter: (a: { checkIn: string }, b: { checkIn: string }) => {
-      return (a.checkIn<b.checkIn)?1 : (a.checkIn===b.checkIn)?0:-1;
-    }
+      return a.checkIn < b.checkIn ? 1 : a.checkIn === b.checkIn ? 0 : -1;
+    },
   },
   {
     title: '근무시간',
@@ -133,57 +133,40 @@ const onChange = (pagination: any, filters: any, sorter: any, extra: any) => {
 };
 
 const HomePage = (_props: any) => {
-
   const [name, setName] = useState('');
-  const [time, setTime] = useState<{ start: Moment; end: Moment }>();
+  const [time, setTime] = useState<Moment>();
+  const [targetData, setTargetData] = useState();
+  useEffect(() => {
+    // 스프레드 시트 api 가 로드되지 않았으면 Skip
+    if (!checkGapi()) {
+      return;
+    }
+    if (!time) {
+      setTargetData(undefined);
+      return;
+    }
+    // 날짜가 선택되면 해당 날짜의 시트를 불러와서 테이블에 보여줌
+    const sheetKey = time.format('YYYY-MM-DD');
+    getSheet(sheetKey).then((sheet) => {
+      setTargetData(converToState(sheet));
+    });
+  }, [time]);
 
-  const newData = useSelector(
+  const rootData = useSelector(
     (state: ApplicationState) => state.inventory.data
   );
-  // 이름, 날짜로 필터링하는 로직 시작
-  const data = (newData ?? [])
-    // 이름이 입력됐을 경우 이름과 매칭하는 유저만 필터링 -> i = newData
-    .filter((i) => (name ? i.user.includes(name) : true))
-    .filter((i) => {
-      // 시간 선택이 안되었을 때 모든 항목을 테이블에 보여줌
-      if (!time) {
-        return true;
-      }
-
-      /*
-       * moment js 가 한글이 포함될 경우 파싱을 못하기 때문에 한글 제거 [.replace(/[가-핳]/g, '')]
-       * 가나다라...처럼 조합된 한글을 찾는 표현식
-       * checkIn, checkOut 값을 파싱 후
-       * 선택한 영역에 포함하는지 검사 -> startOf('day') = day보다 작은 시,분,초를 제외시키기 위함
-       */
-      const checkIn = moment(
-        String(i.checkIn).replace(/[가-핳]/g, ''),
-        'YYYY M DD, hh:mm:ss'
-      ).startOf('second');
-
-      const checkOut = moment(
-        String(i.checkOut).replace(/[가-핳]/g, ''),
-        'YYYY M DD, hh:mm:ss'
-      ).startOf('second');
-
-      // 선택한 시간 범위에 checkIn 시간이 포함되거나
-      // 선택한 시간 범위에 checkOut 시간이 포함되면
-      // 해당 항목을 테이블에 보여줌
-      return (
-        (checkIn.isAfter(time.start) && checkIn.isBefore(time.end)) ||
-        (checkOut.isAfter(time.start) && checkOut.isBefore(time.end))
-      );
-    });      
-
+  const data = (targetData ?? rootData).filter(
+    (i) => !name || i.user.includes(name)
+  );
 
   // 이름, 날짜 필터링 적용을 위해서 UserButton, UserSearch 컴포넌트를 HomePage로 이동
   return (
     <div key={'HP'}>
       <UserButton />
       <HomeDatePicker
-        onChange={(moment: Moment[]) =>
+        onChange={(moment: Moment) =>
           // 시간이 선택되면 moment배열을 start,end프로퍼티로 담는다. 선택되지 않으면 undefined
-          setTime(moment ? { start: moment[0], end: moment[1] } : undefined)
+          setTime(moment)
         }
       />
       <UserSearch onSearch={setName} />
