85b195ddd8fcff31264ce3bbf0be86338cfbc0a9
gkseogus <fbznffldj998@naver.com>
2022-03-19 23:32:58 +0900

    220319_시트생성 로직 구현

---
+++ src/components/GoogleLogin/load-sheet.tsx
@@ -0,0 +1,56 @@
+import moment from 'moment';
+
+export async function loadTodaySheet() {
+  const todayKey = moment().format('YYYY-MM-DD');
+  // 오늘 날짜에 해당하는 시트를 조회
+  const todaySheet = await getSheet(todayKey);
+  if (todaySheet) {
+    return todaySheet;
+  }
+  // 오늘 날짜에 해당하는 시트가 없는 경우 새로 생성
+  await window.gapi.client.sheets.spreadsheets.batchUpdate(
+    { spreadsheetId: '1MCnYjLcdHg7Vu9GUSiOwWxSLDTK__PzNod5mCLnVIwQ' },
+    {
+      requests: [
+        {
+          addSheet: {
+            properties: { title: todayKey },
+          },
+        },
+      ],
+    }
+  );
+  return await getSheet(todayKey);
+}
+
+export function checkGapi() {
+  return window.gapi.client != null;
+}
+
+export async function getSheet(key: string) {
+  const res = await window.gapi.client.sheets.spreadsheets.get({
+    spreadsheetId: '1MCnYjLcdHg7Vu9GUSiOwWxSLDTK__PzNod5mCLnVIwQ',
+    ranges: [],
+    // sheet 목록을 조회할 때 테이블 전체 데이터도 포함
+    includeGridData: true,
+  });
+  // title과 Key 가 매칭되는 시트 찾기
+  return res.result.sheets.find((sheet: any) => sheet.properties.title === key);
+}
+
+export function converToState(sheet: any) {
+  const data = sheet?.data?.[0]?.rowData;
+  if (!data) {
+    return [];
+  }
+  return data.map((row: { values: { formattedValue: string }[] }) => ({
+    key: row.values[7]?.formattedValue,
+    team: row.values[0]?.formattedValue,
+    user: row.values[1]?.formattedValue,
+    checkIn: row.values[2]?.formattedValue,
+    checkOut: row.values[3]?.formattedValue,
+    workTime: row.values[4]?.formattedValue,
+    workState: row.values[5]?.formattedValue,
+    working: row.values[6]?.formattedValue,
+  }));
+}
