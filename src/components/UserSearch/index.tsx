1be98f74670ad7a78b35da7826f969f6197deabd
gkseogus <fbzbffldj@gmail.com>
2022-11-08 17:49:50 +0900

    221108_add to User Search

--- src/components/UserSearch/index.tsx
+++ src/components/UserSearch/index.tsx
@@ -1,42 +1,37 @@
 import { Input, Space, Button } from 'antd';
 import 'antd/dist/antd.css';
 import React, { useState } from 'react';
-import styled from 'styled-components';
 
-const ButtonContainer = styled.div`
-`
-
-const { Search } = Input;
-
-const UserSearch = (_props: any) => {
-  const [size] = useState(8);
+const UserSearch = (props: any) => {
+  const { Search } = Input;
+  const [size] = useState<number>(8);
 
   return (
-    <ButtonContainer key={'US'}>
-      <Space size={size}>
-        <Search
-          placeholder='이름'
-          onSearch={_props.onSearch}
-          enterButton={
-            <Button 
-              style={{
-                top: 14,
-                left: '590px',
-                position: 'fixed',
-                color: 'black',
-              }} 
-            >검색</Button>
-          } 
-          allowClear
-          style={{
-            width: 200,
-            top: 14,
-            left: '380px',
-            position: 'fixed'
-          }}
-        />
-      </Space>
-    </ButtonContainer>
+    <Space size={size}>
+      <Search
+        placeholder="이름"
+        onSearch={props.onSearch}
+        enterButton={
+          <Button
+            style={{
+              top: 14,
+              left: '590px',
+              position: 'fixed',
+              color: 'black',
+            }}
+          >
+            검색
+          </Button>
+        }
+        allowClear
+        style={{
+          width: 200,
+          top: 14,
+          left: '380px',
+          position: 'fixed',
+        }}
+      />
+    </Space>
   );
 };
 
