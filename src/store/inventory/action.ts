7290fa9af04a1e7f99d6e4eb0c0e53a1b79daf1f
gkseogus <fbzbffldj@gmail.com>
2022-11-08 18:50:10 +0900

    221108_add to action

--- src/store/inventory/action.ts
+++ src/store/inventory/action.ts
@@ -1,4 +1,4 @@
-import { FETCH_ERROR,FETCH_SUCCESS, FETCH_UPDATE } from './types';
+import { FETCH_ERROR, FETCH_SUCCESS, FETCH_UPDATE } from './types';
 
 import { ActionCreator, Action, Dispatch } from 'redux';
 import { ThunkAction } from 'redux-thunk';
@@ -6,39 +6,36 @@ import { ThunkAction } from 'redux-thunk';
 import { ApplicationState } from '../index';
 
 export type AppThunk = ActionCreator<
-  ThunkAction<
-   void, 
-   ApplicationState, 
-   null, Action<string>
-  >
+  ThunkAction<void, ApplicationState, null, Action<string>>
 >;
 
-// apiData를 받아와 reducer에게 데이터를 전송
+/** Receive apiData and send data to reducer */
 export const fetchRequest: AppThunk = (item) => {
   return (dispatch: Dispatch): Action => {
     try {
-      return dispatch({ 
-        type: FETCH_SUCCESS,
-        payload: item 
-      });
-    } catch (e) { 
       return dispatch({
-        type: FETCH_ERROR
+        type: FETCH_SUCCESS,
+        payload: item,
+      });
+    } catch (e) {
+      return dispatch({
+        type: FETCH_ERROR,
       });
     }
   };
 };
 
+/** Receive apiData and send update to reducer */
 export const fetchRequestToUpdate: AppThunk = (item) => {
   return (dispatch: Dispatch): Action => {
     try {
-      return dispatch({ 
-        type: FETCH_UPDATE,
-        payload: item 
-      });
-    } catch (e) { 
       return dispatch({
-        type: FETCH_ERROR
+        type: FETCH_UPDATE,
+        payload: item,
+      });
+    } catch (e) {
+      return dispatch({
+        type: FETCH_ERROR,
       });
     }
   };
