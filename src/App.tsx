8654ee38dfd4d6bd085c2d84a4a706b70647aac0
gkseogus <fbzbffldj@gmail.com>
2022-11-08 16:38:41 +0900

    221108_apply to history

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,14 @@ import './App.css';
 import { Provider } from 'react-redux';
 import { ApplicationState } from './store';
 import { Store } from 'redux';
+import { History } from 'history';
 import AllRoutesApp from './routesAll';
-interface StoreFace {
+interface MainProps {
   store: Store<ApplicationState>;
+  history: History;
 }
 
-const App: React.FC<StoreFace> = ({ store }) => {
+const App: React.FC<MainProps> = ({ store, history }) => {
   return (
     <Provider store={store}>
       <AllRoutesApp />
